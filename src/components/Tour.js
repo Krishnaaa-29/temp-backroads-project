9b5c1bc6dfdbb44137fbfbac6d8971734c7c2dd9
Krishnaaa-29 <acctocode29@gmail.com>
2023-06-12 23:35:15 +0530

    first commit

---
+++ src/components/Tour.js
@@ -0,0 +1,27 @@
+const Tour = (tour) => {
+  return (
+    <article className="tour-card">
+      <div className="tour-img-container">
+        <img src={tour.img} className="tour-img" alt={tour.title} />
+        <p className="tour-date">{tour.date}</p>
+      </div>
+      <div className="tour-info">
+        <div className="tour-title">
+          <h4>{tour.title}</h4>
+        </div>
+        <p> {tour.info} </p>
+        <div className="tour-footer">
+          <p>
+            <span>
+              <i className="fas fa-map"></i>
+            </span>{" "}
+            {tour.location}
+          </p>
+          <p>{tour.duration}</p>
+          <p>{tour.cost}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+export default Tour;
